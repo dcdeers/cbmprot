630abec06d38a81f69113a8d246b1eb8164882ce
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2024-11-14 21:55:02 +0800

    update pass structure 14/11 9.54pm

--- cbmprot/lib/screens/password.dart
+++ cbmprot/lib/screens/password.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_password_strength/flutter_password_strength.dart';
-import 'package:password_strength_checker/password_strength_checker.dart';
-import 'dart:math';
 
 class PasswordComparisonScreen extends StatefulWidget {
   @override
-  _PasswordComparisonScreenState createState() => _PasswordComparisonScreenState();
+  _PasswordComparisonScreenState createState() =>
+      _PasswordComparisonScreenState();
 }
 
 class _PasswordComparisonScreenState extends State<PasswordComparisonScreen> {
@@ -18,90 +17,106 @@ class _PasswordComparisonScreenState extends State<PasswordComparisonScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Password Comparison', style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.teal,
+        title: Text(
+          'Password Comparison',
+          style:
+              TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      backgroundColor: Colors.grey.shade900,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildPasswordTextField('Enter Password 1', _obscurePassword1, (value) {
-              setState(() {
-                _password1 = value;
-              });
-            }, () {
-              setState(() {
-                _obscurePassword1 = !_obscurePassword1;
-              });
-            }),
-            SizedBox(height: 20),
-            _buildPasswordTextField('Enter Password 2', _obscurePassword2, (value) {
-              setState(() {
-                _password2 = value;
-              });
-            }, () {
-              setState(() {
-                _obscurePassword2 = !_obscurePassword2;
-              });
-            }),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildPasswordStrengthCard('Password 1 Strength', _password1),
-                _buildPasswordStrengthCard('Password 2 Strength', _password2),
-              ],
-            ),
-            SizedBox(height: 30),
-            if (_password1.isNotEmpty && _password2.isNotEmpty)
-              Center(
-                child: Container(
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.teal.shade700,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.teal.shade400,
-                        blurRadius: 10,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: Text(
-                    _comparePasswords(_password1, _password2),
-                    style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'Montserrat'),
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildPasswordTextField('Enter Password 1', _obscurePassword1,
+                  (value) {
+                setState(() {
+                  _password1 = value;
+                });
+              }, () {
+                setState(() {
+                  _obscurePassword1 = !_obscurePassword1;
+                });
+              }),
+              SizedBox(height: 20),
+              _buildPasswordTextField('Enter Password 2', _obscurePassword2,
+                  (value) {
+                setState(() {
+                  _password2 = value;
+                });
+              }, () {
+                setState(() {
+                  _obscurePassword2 = !_obscurePassword2;
+                });
+              }),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildPasswordStrengthCard('Password 1 Strength', _password1),
+                  _buildPasswordStrengthCard('Password 2 Strength', _password2),
+                ],
+              ),
+              SizedBox(height: 20),
+              if (_password1.isNotEmpty && _password2.isNotEmpty)
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.cyanAccent,
+                      borderRadius: BorderRadius.circular(25), 
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white,
+                          blurRadius: 10,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      _comparePasswords(_password1, _password2),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Montserrat'),
+                    ),
                   ),
                 ),
-              ),
-          ],
+              SizedBox(height: 30),
+              _buildInfoSection(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildPasswordTextField(String labelText, bool obscureText, ValueChanged<String> onChanged, VoidCallback toggleObscureText) {
+  Widget _buildPasswordTextField(String labelText, bool obscureText,
+      ValueChanged<String> onChanged, VoidCallback toggleObscureText) {
     return TextField(
       onChanged: onChanged,
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(25.0)),
         labelText: labelText,
-        labelStyle: TextStyle(fontFamily: 'Montserrat', color: Colors.black),
+        labelStyle: TextStyle(fontFamily: 'Montserrat', color: Colors.white),
         filled: true,
-        fillColor: Colors.teal.shade50,
-        prefixIcon: Icon(Icons.lock, color: Colors.teal),
+        fillColor: Colors.cyanAccent.shade700.withOpacity(0.2),
+        prefixIcon: Icon(Icons.lock, color: Colors.cyanAccent),
         suffixIcon: IconButton(
-          icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility, color: Colors.teal),
+          icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility,
+              color: Colors.cyanAccent),
           onPressed: toggleObscureText,
         ),
       ),
+      style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
       obscureText: obscureText,
     );
   }
@@ -112,7 +127,7 @@ class _PasswordComparisonScreenState extends State<PasswordComparisonScreen> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
-      color: Colors.grey.shade800,
+      color: Colors.black,
       child: Container(
         padding: EdgeInsets.all(20),
         width: 150,
@@ -120,11 +135,20 @@ class _PasswordComparisonScreenState extends State<PasswordComparisonScreen> {
           children: [
             Text(
               title,
-              style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold, fontSize: 16, color: Colors.tealAccent),
+              style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.cyanAccent),
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 10),
-            FlutterPasswordStrength(password: password),
+            FlutterPasswordStrength(
+              password: password,
+              strengthCallback: (strength) {
+                // Optional callback to get the strength value
+              },
+            ),
           ],
         ),
       ),
@@ -157,106 +181,67 @@ class _PasswordComparisonScreenState extends State<PasswordComparisonScreen> {
 
     return score;
   }
-}
 
-class PassphraseGeneratorScreen extends StatefulWidget {
-  @override
-  _PassphraseGeneratorScreenState createState() => _PassphraseGeneratorScreenState();
-}
-
-class _PassphraseGeneratorScreenState extends State<PassphraseGeneratorScreen> {
-  String _passphrase = "";
-  List<String> words = [
-    "apple",
-    "orange",
-    "banana",
-    "cat",
-    "dog",
-    "house",
-    "river",
-    "mountain",
-    "sky",
-    "forest"
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Passphrase Generator', style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.deepPurple,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+  Widget _buildInfoSection() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade700,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
       ),
-      backgroundColor: Colors.grey.shade900,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _passphrase = _generatePassphrase();
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, backgroundColor: Colors.deepPurple,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                shadowColor: Colors.deepPurpleAccent,
-                elevation: 10,
-              ),
-              child: Text('Generate Passphrase', style: TextStyle(fontFamily: 'Montserrat', fontSize: 18)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'What does the Password Comparison feature do?',
+            style: TextStyle(
+              color: Colors.cyanAccent,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
             ),
-            SizedBox(height: 30),
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple.shade50,
-                borderRadius: BorderRadius.circular(25),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.deepPurple.shade200,
-                    blurRadius: 10,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Text(
-                _passphrase,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontFamily: 'Montserrat', color: Colors.deepPurple),
-                textAlign: TextAlign.center,
-              ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'This feature helps you compare two passwords to determine which one is stronger. It assesses the strength of each password based on various factors such as length, uppercase letters, numbers, and special characters.',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'Montserrat',
             ),
-            SizedBox(height: 30),
-            FlutterPasswordStrength(
-              password: _passphrase,
-              strengthCallback: (strength) {
-                // You can use the `strength` value to provide tips to the user
-                print("Passphrase strength: $strength");
-              },
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Tips for a Strong Password:',
+            style: TextStyle(
+              color: Colors.cyanAccent,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '1. Use at least 12 characters for better security.\n'
+            '2. Include both uppercase and lowercase letters.\n'
+            '3. Add numbers and special characters to increase complexity.\n'
+            '4. Avoid using easily guessable information like birthdays or common words.\n'
+            '5. Use a unique password for each of your accounts.',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'Montserrat',
+            ),
+          ),
+        ],
       ),
     );
   }
-
-  String _generatePassphrase() {
-    Random random = Random();
-    List<String> selectedWords = [];
-    for (int i = 0; i < 4; i++) {
-      selectedWords.add(words[random.nextInt(words.length)]);
-    }
-    return "\${selectedWords.join('-')}!\${random.nextInt(100)}";
-  }
 }
-
-
-
