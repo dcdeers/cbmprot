d907c9ebe4d9e611fc89a687030d34abfa6b2df9
5353@laksamanacollege.edu.bn <saidir6g@gmail.com>
2024-11-14 22:57:22 +0800

    fix structure 3 14/11 10.57pm

--- cbmprot/lib/screens/homepage.dart
+++ cbmprot/lib/screens/homepage.dart
@@ -8,22 +8,20 @@ import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   // Static data lists
-
-  // Add a list for the text labels
   final List<String> catNames = [
     "QUIZ",
     'TIPS',
     'LEARN',
     'NEWS',
-    'COMPARER',
+    'STRENGTH TESTER',
   ];
 
   final List<Icon> catIcons = [
-    Icon(LineAwesomeIcons.list_alt_solid, color: Colors.white, size: 55),
-    Icon(LineAwesomeIcons.clipboard_check_solid, color: Colors.white, size: 55),
-    Icon(LineAwesomeIcons.brain_solid, color: Colors.white, size: 55),
-    Icon(LineAwesomeIcons.newspaper_solid, color: Colors.white, size: 55),
-    Icon(LineAwesomeIcons.fingerprint_solid, color: Colors.white, size: 55),
+    Icon(LineAwesomeIcons.list_alt_solid, color: Colors.white, size: 50),
+    Icon(LineAwesomeIcons.clipboard_check_solid, color: Colors.white, size: 50),
+    Icon(LineAwesomeIcons.brain_solid, color: Colors.white, size: 50),
+    Icon(LineAwesomeIcons.newspaper_solid, color: Colors.white, size: 50),
+    Icon(LineAwesomeIcons.fingerprint_solid, color: Colors.white, size: 50),
   ];
 
   @override
@@ -34,39 +32,30 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            // Image Container
+            // Logo Section
             Container(
-              height: 110,
-              width: 110,
+              height: 120,
+              width: 120,
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('images/2.png'), // Replace with your image path
                 ),
               ),
             ),
-            SizedBox(height: 5),
-
-            // Title
-            Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.black,
-              ),
-              child: Center(
-                child: Text(
-                  "CYBERMIND",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 48, // Reduced size to fit smaller screens
-                    fontFamily: 'IBMPlexMono',
-                    letterSpacing: 2.0,
-                  ),
-                ),
+            SizedBox(height: 10),
+            // Title Section
+            Text(
+              "CYBERMIND",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 42,
+                fontFamily: 'IBMPlexMono',
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 10),
-
-            // Menu Items
+            // Menu Section
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -76,40 +65,37 @@ class HomePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Expanded(
-                        child: _buildMenuItem(
-                            context, catNames[0], catIcons[0], CategoryScreen()),
+                        child: _buildMenuItem(context, catNames[0], catIcons[0], CategoryScreen()),
                       ),
                       SizedBox(width: 15),
                       Expanded(
-                        child: _buildMenuItem(context, catNames[1], catIcons[1],
-                            AwarenessScreen()), // Replace with your actual screen
+                        child: _buildMenuItem(context, catNames[1], catIcons[1], AwarenessScreen()),
                       ),
                     ],
                   ),
-                  SizedBox(height: 15),
-
-                  // Comparer Button (Centered)
-                  _buildMenuItem(
-                    context,
-                    catNames[2],
-                    catIcons[2],
-                    LearnScreen(), // Replace with your actual screen
-                    width: MediaQuery.of(context).size.width * 0.44,
-                  ),
-                  SizedBox(height: 15),
-
-                  // Third Row: Learn and News
+                  // Second Row: Learn and News
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Expanded(
-                        child: _buildMenuItem(context, catNames[3], catIcons[3],
-                            CybersecurityNewsScreen()),
+                        child: _buildMenuItem(context, catNames[2], catIcons[2], LearnScreen()),
                       ),
                       SizedBox(width: 15),
                       Expanded(
-                        child: _buildMenuItem(context, catNames[4], catIcons[4],
-                            PasswordComparisonScreen()), // Replace with your actual screen
+                        child: _buildMenuItem(context, catNames[3], catIcons[3], CybersecurityNewsScreen()),
+                      ),
+                    ],
+                  ),
+                  // Centered Row: Comparer button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildMenuItem(
+                        context,
+                        catNames[4],
+                        catIcons[4],
+                        PasswordComparisonScreen(),
+                        width: MediaQuery.of(context).size.width * 0.5,
                       ),
                     ],
                   ),
@@ -122,8 +108,8 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildMenuItem(BuildContext context, String name, Icon icon,
-      Widget screen, {double? width}) {
+  Widget _buildMenuItem(
+      BuildContext context, String name, Icon icon, Widget screen, {double? width}) {
     return InkWell(
       onTap: () {
         Navigator.push(
@@ -132,29 +118,38 @@ class HomePage extends StatelessWidget {
         );
       },
       child: Container(
-        width: 160,  // Fixed width for the button
-        height: 160, // Fixed height for the button
+        width: width ?? 170, // Fixed width for the button or custom width if provided
+        height: 170, // Fixed height for the button
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
         decoration: BoxDecoration(
+          color: Color.fromRGBO(0, 194, 203, 100), // Adjusted alpha value for opacity to 1 (fully opaque)
           borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.white, width: 4),
-          color: Color.fromRGBO(0, 194, 203, 100),
+          border: Border.all(
+            color: Colors.white, // Border color
+            width: 4, // Border width
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.4),
+              blurRadius: 10,
+              offset: Offset(0, 4),
+            ),
+          ],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: icon,
-            ),
-            SizedBox(height: 10),
+            icon,
+            SizedBox(height: 15),
             Text(
               name,
+               textAlign: TextAlign.center, // Ensure the text is centered
               style: TextStyle(
                 fontFamily: 'IBMPlexMono',
-                fontSize: 20, // Reduced size for better fit
+                fontSize: 20,
                 color: Colors.white,
-                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
               ),
             ),
           ],
